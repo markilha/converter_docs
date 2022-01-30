436269229b612707b357676db78bdc369b24e6c6
Markilha <markilha@gmail.com>
2022-01-30 15:50:52 -0300

    Subiu projeto

---
+++ ConverterDocumento/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ConverterDocumento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboPadraoArquivos = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbArquivos = new System.Windows.Forms.ListBox();
+            this.fbd1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnJuntar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Controls.Add(this.cboPadraoArquivos);
+            this.groupBox1.Location = new System.Drawing.Point(42, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(713, 381);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // cboPadraoArquivos
+            // 
+            this.cboPadraoArquivos.FormattingEnabled = true;
+            this.cboPadraoArquivos.Location = new System.Drawing.Point(28, 32);
+            this.cboPadraoArquivos.Name = "cboPadraoArquivos";
+            this.cboPadraoArquivos.Size = new System.Drawing.Size(136, 21);
+            this.cboPadraoArquivos.TabIndex = 0;
+            this.cboPadraoArquivos.Text = "DOCX Files (*.docx)";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(170, 32);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(103, 22);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbArquivos);
+            this.groupBox2.Location = new System.Drawing.Point(28, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(655, 271);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Arquivos";
+            // 
+            // lbArquivos
+            // 
+            this.lbArquivos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbArquivos.FormattingEnabled = true;
+            this.lbArquivos.Location = new System.Drawing.Point(3, 16);
+            this.lbArquivos.Name = "lbArquivos";
+            this.lbArquivos.Size = new System.Drawing.Size(649, 252);
+            this.lbArquivos.TabIndex = 0;
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(585, 422);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(170, 35);
+            this.btnConverter.TabIndex = 2;
+            this.btnConverter.Text = "Converter ";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(31, 347);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(649, 21);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // btnJuntar
+            // 
+            this.btnJuntar.Location = new System.Drawing.Point(409, 422);
+            this.btnJuntar.Name = "btnJuntar";
+            this.btnJuntar.Size = new System.Drawing.Size(170, 35);
+            this.btnJuntar.TabIndex = 3;
+            this.btnJuntar.Text = "Mesclar";
+            this.btnJuntar.UseVisualStyleBackColor = true;
+            this.btnJuntar.Click += new System.EventHandler(this.btnJuntar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 469);
+            this.Controls.Add(this.btnJuntar);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Converter Arquivos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lbArquivos;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ComboBox cboPadraoArquivos;
+        private System.Windows.Forms.FolderBrowserDialog fbd1;
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnJuntar;
+    }
+}
+
---
+++ ConverterDocumento/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace ConverterDocumento
+{
+    public partial class Form1 : Form
+    {
+        public static Microsoft.Office.Interop.Word.Document wordDocument { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {           
+try
+            {
+                string caminho = "";
+
+                if (fbd1.ShowDialog() == DialogResult.OK)
+                {
+                    caminho = fbd1.SelectedPath;
+                }
+
+                ProcuraArquivos(lbArquivos, caminho, cboPadraoArquivos.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static void ProcuraArquivos(ListBox lstb, string pastaInicial, string padrao)
+        {
+
+            // Limpa a listbox
+            lstb.Items.Clear();
+
+            // Define os padrões
+            string[] padroes = AnalisaPadroes(padrao);
+            DirectoryInfo dir_info = new DirectoryInfo(pastaInicial);
+            //Procura no diretório usando os padrões
+            ProcuraDiretorio(lstb, dir_info, padroes);
+            //exibe mensagem 
+            MessageBox.Show("Foram encontrados " + lstb.Items.Count + " arquivos.");
+
+        }
+        private static void ProcuraDiretorio(ListBox lstb, DirectoryInfo dir_info, string[] padroes)
+        {
+            try
+            {
+                // Procura o diretorio
+                foreach (string padrao in padroes)
+                {
+                    // Verifica o padrão
+                    foreach (FileInfo Arq_info in dir_info.GetFiles(padrao))
+                    {
+                        // Processa o arquivo
+
+                        ProcessaArquivo(lstb, Arq_info);
+                    }
+                }
+
+                // Procura nos subdiretorios
+                foreach (DirectoryInfo subdir_info in dir_info.GetDirectories())
+                {
+                    ProcuraDiretorio(lstb, subdir_info, padroes);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private static string[] AnalisaPadroes(string string_padrao)
+        {
+            try
+            {
+                // Pega tudo que esta entre os parenteses
+                if (string_padrao.Contains("("))
+                {
+
+                    string_padrao = TextoEntre(string_padrao, "(", ")");
+                }
+
+                // divide a string em ;
+                string[] resultado = string_padrao.Split(';');
+
+                // Remove todos os padrões para espaço extra
+                for (int i = 0; i < resultado.Length; i++)
+                {
+                    resultado[i] = resultado[i].Trim();
+                }
+                //retorna o resultado
+                return resultado;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        private static string TextoEntre(string txt, string delimitador1, string delimitador2)
+        {
+            try
+            {
+                // Encontra o delimitador inicial
+                int pos1 = txt.IndexOf(delimitador1);
+                int texto_inicio = pos1 + delimitador1.Length;
+                int pos2 = txt.IndexOf(delimitador2, texto_inicio);
+                return txt.Substring(texto_inicio, pos2 - texto_inicio);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        private static void ProcessaArquivo(ListBox lstb, FileInfo arq_info)
+        {
+            try
+            {
+                //exibe o nome do arquivo no listbox
+                lstb.Items.Add(arq_info.FullName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao processar o arquivo " + arq_info.FullName + "\n" + ex.Message);
+            }
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int cont = 0;
+                foreach (string caminho in lbArquivos.Items)
+                {
+                    string pdfSaida = caminho.Replace("docx", "pdf");                 
+                    Microsoft.Office.Interop.Word.Application appWord = new Microsoft.Office.Interop.Word.Application();
+                    wordDocument = appWord.Documents.Open(caminho);
+                    wordDocument.ExportAsFixedFormat(pdfSaida, WdExportFormat.wdExportFormatPDF);
+                    appWord.Quit();
+                    wordDocument.Close();
+                    cont++;
+                    BarraProgresso(progressBar1, lbArquivos.Items.Count, cont);
+                }
+                   
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static string BarraProgresso(ProgressBar processo, int progressBarMaximo, int contagem)
+        {
+            double porc = 0;
+            processo.Maximum = progressBarMaximo;
+
+            if (contagem < progressBarMaximo)
+            {
+                contagem += 1;
+                processo.Value = contagem;
+                porc = ((100 * processo.Value) / processo.Maximum);
+
+            }
+
+            return porc.ToString();
+        }
+
+        private void btnJuntar_Click(object sender, EventArgs e)
+        {
+            int cont = 0;
+            try
+            {
+                PdfDocument destino = new PdfDocument();
+
+                foreach (string caminho in lbArquivos.Items)
+                {
+
+                    using (PdfDocument origem = PdfReader.Open(caminho, PdfDocumentOpenMode.Import))
+                    {
+                        int count = origem.PageCount;
+                        for (int i = 0; i < count; i++)
+                        {
+                            PdfPage page = origem.Pages[i];
+                            destino.AddPage(page);
+                        }
+                        cont++;
+
+                        BarraProgresso(progressBar1, lbArquivos.Items.Count, cont);
+
+                    }
+                }
+
+                destino.Save(CaminhoSalva("PDF|*.pdf", "", "pdfFinal.pdf"));
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        public static string CaminhoSalva(string filtro, string diretorio, string nome)
+        {
+            string caminho = "";
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            fileDialog.Filter = filtro;
+            fileDialog.InitialDirectory = diretorio;
+            fileDialog.Title = "Selecione o caminho para salvar";
+            fileDialog.FileName = nome;
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                caminho = fileDialog.FileName; 
+
+            }
+
+            return caminho;
+        }
+    }
+}
